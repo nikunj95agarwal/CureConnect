027ea4432650c9d05a9c83edc322d36573bbea2f
NIKUNJ AGARWAL <133116644+nikunj95agarwal@users.noreply.github.com>
2024-07-11 19:37:25 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // config.js
-export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://cure-connect-backend.onrender.com/api/v1";
 
 export const token = localStorage.getItem("token") ;
 console.log("Token in config.js:", token); // Log token in config.js
